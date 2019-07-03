5c35d227c8246f9d1f28fefc7f5167171727c68e
nakochan <eunnamu094@gmail.com>
2019-07-03 21:39:41 +0900

    update 50

--- frontend/components/sideMenu.vue
+++ frontend/components/sideMenu.vue
@@ -8,23 +8,19 @@
       <el-menu-item index='2' @click='move("/b/all")'>
         <span>전체</span>
       </el-menu-item>
-      <el-menu-item index='4' @click='move("/b/girl")'>
-        <span>연예</span>
+      <el-menu-item index='3' @click='move("/b/talk")'>
+        <span>토크</span>
       </el-menu-item>
-      <el-menu-item index='5' @click='move("/b/anime")'>
-        <span>애니</span>
+      <el-menu-item index='4' @click='move("/b/feedback")'>
+        <span>건의</span>
+      </el-menu-item>
+      <el-menu-item index='5' @click='move("/b/notice")'>
+        <span>공지사항</span>
       </el-menu-item>
       <el-submenu index='6'>
-        <template slot='title'>게시판</template>
-        <el-menu-item index='6-1' @click='move("/b/talk")'>토크</el-menu-item>
-        <el-menu-item index='6-2' @click='move("/b/history")'>역사</el-menu-item>
-        <el-menu-item index='6-3' @click='move("/b/feedback")'>건의</el-menu-item>
-        <el-menu-item index='6-4' @click='move("/b/notice")'>공지</el-menu-item>
-      </el-submenu>
-      <el-submenu index='7'>
         <template slot='title'>포인트</template>
-        <el-menu-item index='7-1' @click='move("/sticker")'>스티커샵</el-menu-item>
-        <el-menu-item index='7-2' @click='move("/iconshop")'>아이콘샵</el-menu-item>
+        <el-menu-item index='6-1' @click='move("/sticker")'>스티커샵</el-menu-item>
+        <el-menu-item index='6-2' @click='move("/iconshop")'>아이콘샵</el-menu-item>
       </el-submenu>
     </el-menu>
   </div>

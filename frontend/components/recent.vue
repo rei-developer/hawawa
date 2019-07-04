ee78a0cc2a25c4f375d5a1ce3767d4d3f378e8f9
nakochan <eunnamu094@gmail.com>
2019-07-04 15:37:48 +0900

    recent

--- frontend/components/recent.vue
+++ frontend/components/recent.vue
@@ -27,7 +27,7 @@
       </div>
     </div>
     <div class='ADSidebar'>
-      <iframe src='/ad-sidebar.html' />
+      <iframe width='250' height='250' allowtransparency='true' src='https://tab2.clickmon.co.kr/pop/wp_ad_250.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1058998%7C%5E%7CAdver_M_1046207&mon_rf=REFERRER_URL' frameborder='0' scrolling='no' />
     </div>
   </div>
 </template>

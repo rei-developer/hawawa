6b94edfa284bde6f02f028386521b752fcc6061e
Unyeong Baek <eunnamu094@gmail.com>
2019-08-24 19:55:55 +0900

    remove updater

--- updater/src/database/update.js
+++
@@ -1,8 +0,0 @@
-const pool = require('./index')
-
-module.exports = async (hits, topicId) => {
-  await pool.query(
-    `UPDATE TopicCounts SET hits = hits + ? WHERE topicId = ?`,
-    [hits, topicId]
-  )
-}
